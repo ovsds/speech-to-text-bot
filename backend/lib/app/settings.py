a14ebf97645e2436837b5cef4909aaf340a1a6c0
ovsds <ovsds@yandex-team.ru>
2025-01-09 16:33:53 +0100

    ci: add docker auxiliary service

--- backend/lib/app/settings.py
+++ backend/lib/app/settings.py
@@ -1,14 +1,12 @@
-import os
-import typing
 import warnings
 
 import pydantic
-import pydantic_settings
 
 import lib.utils.logging as logging_utils
+import lib.utils.pydantic as pydantic_utils
 
 
-class AppSettings(pydantic.BaseModel):
+class AppSettings(pydantic_utils.BaseSettingsModel):
     env: str = "production"
     debug: bool = False
     version: str = "unknown"
@@ -25,18 +23,18 @@ class AppSettings(pydantic.BaseModel):
         return self.debug
 
 
-class LoggingSettings(pydantic.BaseModel):
+class LoggingSettings(pydantic_utils.BaseSettingsModel):
     level: logging_utils.LogLevel = "INFO"
     format: str = "%(asctime)s | %(name)s | %(levelname)s | %(message)s"
 
 
-class ServerSettings(pydantic.BaseModel):
+class ServerSettings(pydantic_utils.BaseSettingsModel):
     host: str = "localhost"
     port: int = 8080
     public_host: str = NotImplemented
 
 
-class TelegramSettings(pydantic.BaseModel):
+class TelegramSettings(pydantic_utils.BaseSettingsModel):
     token: str = NotImplemented
     bot_name: str = "SpeechToTextBot"
     bot_short_description: str = "Speech to text bot"
@@ -49,55 +47,13 @@ class TelegramSettings(pydantic.BaseModel):
     webhook_secret_token: str = NotImplemented
 
 
-class Settings(pydantic_settings.BaseSettings):
+class Settings(pydantic_utils.BaseSettings):
     app: AppSettings = pydantic.Field(default_factory=AppSettings)
     logs: LoggingSettings = pydantic.Field(default_factory=LoggingSettings)
     server: ServerSettings = pydantic.Field(default_factory=ServerSettings)
     telegram: TelegramSettings = pydantic.Field(default_factory=TelegramSettings)
 
     thread_pool_executor_max_workers: int = 10
-
-    model_config = pydantic_settings.SettingsConfigDict(
-        env_nested_delimiter="__",
-    )
-
-    @classmethod
-    def settings_customise_sources(
-        cls,
-        settings_cls: type[pydantic_settings.BaseSettings],
-        init_settings: pydantic_settings.PydanticBaseSettingsSource,
-        env_settings: pydantic_settings.PydanticBaseSettingsSource,
-        dotenv_settings: pydantic_settings.PydanticBaseSettingsSource,
-        file_secret_settings: pydantic_settings.PydanticBaseSettingsSource,
-    ) -> tuple[pydantic_settings.PydanticBaseSettingsSource, ...]:
-        return (
-            env_settings,
-            *cls.settings_yaml_sources(settings_cls),
-        )
-
-    @classmethod
-    def settings_yaml_sources(
-        cls,
-        settings_cls: type[pydantic_settings.BaseSettings],
-    ) -> typing.Sequence[pydantic_settings.YamlConfigSettingsSource]:
-        setting_yaml_env = os.environ.get("SETTINGS_YAML", None)
-
-        if setting_yaml_env is None:
-            return []
-
-        paths = setting_yaml_env.split(":")
-
-        for path in paths:
-            if not os.path.exists(path):
-                raise FileNotFoundError(f"Settings file not found: {path}")
-
-        return [
-            pydantic_settings.YamlConfigSettingsSource(
-                settings_cls,
-                yaml_file=path,
-            )
-            for path in paths
-        ]
 
 
 __all__ = [
